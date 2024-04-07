80d97c32641efbf063e95ba54627908296a5c277
EduardoLamatta <eduardolamattap@gmail.com>
2024-04-06 22:06:51 -0500

    Palindromos y Suma de numeros consecutivos

---
+++ EjercicioSumaNumerosConsecutivosAarray/EjercicioSumaNumerosConsecutivosAarray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EjercicioSumaNumerosConsecutivosArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 12, 30, 43, 79, 12, 34, 1, 76, 54, 45};
+            AddNumbers(numbers);
+        }
+
+        static void AddNumbers(int[] _numbers)
+        {
+            int[] sumNumbers = new int[_numbers.Length - 1];
+            int[] numbersOrder = new int[sumNumbers.Length];
+            int count = 0;
+
+            for (int i = 0; i < _numbers.Length - 1; i++)
+            {
+                sumNumbers[i] = _numbers[i] + _numbers[i + 1];
+            }
+
+            foreach (int n in sumNumbers)
+            {
+                for (int i = 0; i < sumNumbers.Length; i++)
+                {
+                    if (n < sumNumbers[i]) count++;
+                }
+                numbersOrder[count] = n;
+                count = 0;
+            }
+            Console.WriteLine("la suma mas alta de dos numeros adyacente es: " + numbersOrder[0]);
+        }
+
+    }
+}
---
+++ ejerciciopalindromo/ejerciciopalindromo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ejercicioPalindromo
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string initialWord;
+            string wordInverse = "";
+
+            initialWord = Console.ReadLine();
+
+            List<char> letters = new List<char>(initialWord);
+
+            for (int i = 0; i < letters.Count; i++)
+            {
+                wordInverse += letters[letters.Count - 1 - i];
+            }
+          
+            Console.WriteLine(wordInverse);
+
+
+        }
+
+
+    }
+}
