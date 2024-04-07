5f384b5355a9e09532ce0d28f89c70832a40d5f8
EduardoLamatta <eduardolamattap@gmail.com>
2024-04-07 02:12:46 -0500

    In English

--- Cipher.cs
+++ Cipher.cs
@@ -5,159 +5,152 @@ namespace ProgramaDesencriptar
 {
     class Desencriptar
     {
+        //Ejercicio extraido de BamtangGames.com
+
         static void Main(string[] args)
         {
-            string enunciado = "Bgc-bfufb tegaedppqna ql aggv zge xof tegaedppfe'l lgjb.\r\n\r\nXof adpf vflqanfe logjbvn'x hf pdwqna d cgebv qn coqro xof tbdkfe ql mjlx d lpdbb tdex. Xof tbdkfe QL XOF HGLL; qx'l kgje vjxk xg fnxfexdqn oqp ge ofe.\r\n\r\nZgrjl ql d pdxxfe gz vfrqvqna codx xoqnal kgj def ngx agqna xg vg.\r\n\r\nXof rglx gz dvvqna d zfdxjef qln'x mjlx xof xqpf qx xdwfl xg rgvf qx. Xof rglx dblg qnrbjvfl xof dvvqxqgn gz dn ghlxdrbf xg zjxjef fstdnlqgn. Xof xeqrw ql xg tqrw xof zfdxjefl xodx vgn'x zqaox fdro gxofe. - Mgon Rdepdrw.\r\n\r\n(ccc.adpdljxed.rgp/uqfc/nfcl/234346?utkjpvbjr)\r\n\r\n(ccc.hedqnkijgxf.rgp/ijgxfl/djxogel/m/mgon_rdepdrw.oxpb)";
-            List<char> lista = new List<char>(enunciado);
-            String[] invalidChars = new string[] { " ", "-", "_", ",", ";", ".", "/", "(", ")", "\r", "\n", "3", "4", "'", "?", "0", "2", "6" , "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-    "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            string statement = "Bgc-bfufb tegaedppqna ql aggv zge xof tegaedppfe'l lgjb.\r\n\r\nXof adpf vflqanfe logjbvn'x hf pdwqna d cgebv qn coqro xof tbdkfe ql mjlx d lpdbb tdex. Xof tbdkfe QL XOF HGLL; qx'l kgje vjxk xg fnxfexdqn oqp ge ofe.\r\n\r\nZgrjl ql d pdxxfe gz vfrqvqna codx xoqnal kgj def ngx agqna xg vg.\r\n\r\nXof rglx gz dvvqna d zfdxjef qln'x mjlx xof xqpf qx xdwfl xg rgvf qx. Xof rglx dblg qnrbjvfl xof dvvqxqgn gz dn ghlxdrbf xg zjxjef fstdnlqgn. Xof xeqrw ql xg tqrw xof zfdxjefl xodx vgn'x zqaox fdro gxofe. - Mgon Rdepdrw.\r\n\r\n(ccc.adpdljxed.rgp/uqfc/nfcl/234346?utkjpvbjr)\r\n\r\n(ccc.hedqnkijgxf.rgp/ijgxfl/djxogel/m/mgon_rdepdrw.oxpb)";
+            List<char> listChars = new List<char>(statement);
+            String[] invalidChars = new string[] { " ", "-", "_", ",", ";", ".", "/", "(", ")", "\r", "\n", "3", "4", "'", "?", "0", "2", "6" , "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
             Dictionary<string, int > dictNumbers = new Dictionary<string, int>();
             //string freqLang = "TEOAISRHNUCMDLGWFPYKJBVQX";
-            string mensaje = string.Empty;
+            string message = string.Empty;
 
             //List<char> freqLangList = new List<char>(freqLang);
 
-            int count = 0, count_2 = 0;
+            int repeatCounter = 0, invalidCounter = 0;
 
-            foreach (char c in lista)
+            foreach (char c in listChars)
             {
-                for (int i = 0; i < lista.Count; i++)
+                for (int i = 0; i < listChars.Count; i++)
                 {
-                    if (c == lista[i]) count++;
+                    if (c == listChars[i]) repeatCounter++;
                 }
                 for (int i = 0; i < invalidChars.Length; i++)
                 {
-                    if (c.ToString() == invalidChars[i]) count_2++;
+                    if (c.ToString() == invalidChars[i]) invalidCounter++;
                 }
 
-                if (!dictNumbers.ContainsKey(c.ToString()) && count_2 == 0)
+                if (!dictNumbers.ContainsKey(c.ToString()) && invalidCounter == 0)
                 {
-                    dictNumbers.Add(c.ToString(), count);
+                    dictNumbers.Add(c.ToString(), repeatCounter);
                 }
                 
-                count = 0;
-                count_2 = 0;
+                repeatCounter = 0;
+                invalidCounter = 0;
             }
             
-            var valoresOrdenados = dictNumbers.OrderByDescending(pair => pair.Value);
+            var orderValues = dictNumbers.OrderByDescending(pair => pair.Value);
 
-          /* Console.WriteLine(dictNumbers.Count);
-
-            foreach (var par in valoresOrdenados)
-            {
-                Console.WriteLine($"Clave: {par.Key}, Valor: {par.Value}");
-            }*/
-
-
-            foreach(char ch in enunciado)
+            foreach(char ch in statement)
             {   
                 if (ch == 'x' || ch == 'X')
                 {
-                    mensaje += 't';
+                    message += 't';
                 }
                 
                 else if (ch == 'f' || ch == 'F')
                 {
-                    mensaje += 'e';
+                    message += 'e';
                 }
                 else if (ch == 'g' || ch == 'G')
                 {
-                    mensaje += 'o';
+                    message += 'o';
                 }
                 else if (ch == 'd' || ch == 'D')
                 {
-                    mensaje += 'a';
+                    message += 'a';
                 }
                 else if (ch == 'q' || ch == 'Q')
                 {
-                    mensaje += 'i';
+                    message += 'i';
                 }
                 else if (ch == 'e' || ch == 'E')
                 {
-                    mensaje += 'r';
+                    message += 'r';
                 }
                 else if (ch == 'l' || ch == 'L')
                 {
-                    mensaje += 's';
+                    message += 's';
                 }
                 else if (ch == 'o' || ch == 'O')
                 {
-                    mensaje += 'h';
+                    message += 'h';
                 }
                 else if (ch == 'n' || ch == 'N')
                 {
-                    mensaje += 'n';
+                    message += 'n';
                 }
                 else if (ch == 'j' || ch == 'J')
                 {
-                    mensaje += 'u';
+                    message += 'u';
                 }
                 else if (ch == 'p' || ch == 'P')
                 {
-                    mensaje += 'm';
+                    message += 'm';
                 }
                 else if (ch == 'r' || ch == 'R')
                 {
-                    mensaje += 'c';
+                    message += 'c';
                 }
                 else if (ch == 'v' || ch == 'V')
                 {
-                    mensaje += 'd';
+                    message += 'd';
                 }
                 else if (ch == 'b' || ch == 'B')
                 {
-                    mensaje += 'l';
+                    message += 'l';
                 }
                 else if (ch == 'a' || ch == 'A')
                 {
-                    mensaje += 'g';
+                    message += 'g';
                 }
                 else if (ch == 'c' || ch == 'C')
                 {
-                    mensaje += 'w';
+                    message += 'w';
                 }
                 else if (ch == 't' || ch == 'T')
                 {
-                    mensaje += 'p';
+                    message += 'p';
                 }
                 else if (ch == 'z' || ch == 'Z')
                 {
-                    mensaje += 'f';
+                    message += 'f';
                 }
                 else if (ch == 'k' || ch == 'K')
                 {
-                    mensaje += 'y';
+                    message += 'y';
                 }
                 else if (ch == 'w' || ch == 'W')
                 {
-                    mensaje += 'k';
+                    message += 'k';
                 }
                 else if (ch == 'm' || ch == 'M')
                 {
-                    mensaje += 'j';
+                    message += 'j';
                 }
                 else if (ch == 'u' || ch == 'U')
                 {
-                    mensaje += 'v';
+                    message += 'v';
                 }
                 else if (ch == 'h' || ch == 'H')
                 {
-                    mensaje += 'b';
+                    message += 'b';
                 }
                 else if (ch == 'i' || ch == 'I')
                 {
-                    mensaje += 'q';
+                    message += 'q';
                 }
                 else if (ch == 's' || ch == 'S')
                 {
-                    mensaje += 'x';
+                    message += 'x';
                 }
                 else
                 {
-                    mensaje += ch;
+                    message += ch;
                 }
             }
-            Console.WriteLine(mensaje);
+            Console.WriteLine(message);
 
 
         }
