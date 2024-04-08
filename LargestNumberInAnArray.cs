ef6ad94223089be1ea6a8a9827178cf21c52125c
EduardoLamatta <eduardolamattap@gmail.com>
2024-04-07 20:47:49 -0500

    Largest Number in Array

---
+++ LargestNumberInAnArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Program
+{
+    class HightestNumber
+    {
+        //Interaging an array one time find the largest number
+        static void Main(string[] args)
+        {
+            List<double> numbersList = new List<double> { 10.5, 20.3, 15.7, 8.2, 30.1, 25.6, 12.9, 18.4, 22.0, 27.8};
+
+            double number = 0;
+            for (int i = 0; i < numbersList.Count - 1; i++)
+            {
+                if (numbersList[i] > numbersList[i + 1] && numbersList[i] > number)
+                {
+                    number = numbersList[i];
+                }
+            }
+
+            Console.WriteLine(number);
+
+        }
+    }
+}
+
+
+
+
+
